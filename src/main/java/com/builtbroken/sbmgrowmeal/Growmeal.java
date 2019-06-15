4fdca3ccccbd6c1e0d3a4432d0021667b1ecf2f8
Robert WC Seifert <rseifert.phone@gmail.com>
2019-06-15 15:59:15 -0400

    Slightly rewrite of logic and separate into its own class

--- src/main/java/com/builtbroken/sbmgrowmeal/Growmeal.java
+++ src/main/java/com/builtbroken/sbmgrowmeal/Growmeal.java
@@ -1,18 +1,8 @@
 package com.builtbroken.sbmgrowmeal;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.IGrowable;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUseContext;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -21,70 +11,23 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.ObjectHolder;
 
 @Mod(Growmeal.MODID)
-@EventBusSubscriber(bus=Bus.MOD)
+@EventBusSubscriber(bus = Bus.MOD)
 public class Growmeal
 {
     public static final String MODID = "sbmgrowmeal";
     public static final String NAME = "[SBM] Growmeal";
     public static final String PREFIX = MODID + ":";
 
-    @ObjectHolder(Growmeal.PREFIX + "growmeal")
+    public static final String ITEM_NAME = "growmeal";
+
+    @ObjectHolder(Growmeal.PREFIX + ITEM_NAME)
     public static final Item GROWMEAL = null;
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event)
     {
-        event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MISC)) {
-            @Override
-            public ActionResultType onItemUse(ItemUseContext ctx)
-            {
-                World world = ctx.getWorld();
-                BlockPos pos = ctx.getPos();
-                PlayerEntity player = ctx.getPlayer();
-                Hand hand = null;
-                ItemStack stack = ctx.getItem();
-
-                //find out which hand the item is held in to swing the arm later on
-                if(player.getItemStackFromSlot(EquipmentSlotType.MAINHAND).getItem() == GROWMEAL)
-                    hand = Hand.MAIN_HAND;
-                else if(player.getItemStackFromSlot(EquipmentSlotType.OFFHAND).getItem() == GROWMEAL)
-                    hand = Hand.OFF_HAND;
-
-                //world.getBlockState(pos) is never saved as the state will not update otherwhise after applying the meal
-                if(world.getBlockState(pos).getBlock() instanceof IGrowable)
-                {
-                    if(!world.isRemote)
-                    {
-                        boolean hasGrown = false;
-
-                        for(int i = 0; i < 1000; i++) //try a thousand times to not have an infinite loop with a growable that can grow infinite times
-                        {
-                            Block growable = world.getBlockState(pos).getBlock();
-
-                            if(growable instanceof IGrowable && ((IGrowable)growable).canGrow(world, pos, world.getBlockState(pos), world.isRemote))
-                            {
-                                ((IGrowable)growable).grow(world, world.rand, pos, world.getBlockState(pos));
-                                hasGrown = true;
-                            }
-                            else break; //no need to try even more when the growable can't grow
-                        }
-
-                        if(hasGrown) //make sure that the growable has grown before removing an item from the player
-                        {
-                            world.playEvent(2005, pos, 0); //particles
-
-                            if(!player.isCreative())
-                                stack.shrink(1);
-
-                            return ActionResultType.SUCCESS;
-                        }
-                    }
-
-                    player.swingArm(hand); //swings according to this method's return value
-                }
-
-                return ActionResultType.PASS;
-            }
-        }.setRegistryName(new ResourceLocation(Growmeal.MODID, "growmeal")));
+        event.getRegistry().register(new ItemGrowmeal(new Item.Properties().group(ItemGroup.MISC))
+                .setRegistryName(new ResourceLocation(Growmeal.MODID, ITEM_NAME)));
     }
+
 }
---
+++ src/main/java/com/builtbroken/sbmgrowmeal/ItemGrowmeal.java
@@ -0,0 +1,92 @@
+package com.builtbroken.sbmgrowmeal;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.IGrowable;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+/**
+ * Created by Dark(DarkGuardsman, Robert) on 6/15/2019.
+ */
+public class ItemGrowmeal extends Item
+{
+    public static int tries = 1000;
+
+    public ItemGrowmeal(Properties properties)
+    {
+        super(properties);
+    }
+
+    @Override
+    public ActionResultType onItemUse(ItemUseContext ctx)
+    {
+        final World world = ctx.getWorld();
+        final BlockPos pos = ctx.getPos();
+        final PlayerEntity player = ctx.getPlayer();
+        final Hand hand = ctx.func_221531_n();
+        final ItemStack stack = ctx.getItem();
+
+        //world.getBlockState(pos) is never saved as the state will not update otherwhise after applying the meal
+        if (isValidToApply(world, pos))
+        {
+            if (!world.isRemote)
+            {
+                if (cycleGrowth(world, pos)) //make sure that the growable has grown before removing an item from the player
+                {
+                    world.playEvent(2005, pos, 0); //particles
+
+                    if (!player.isCreative())
+                    {
+                        stack.shrink(1);
+                    }
+
+                    return ActionResultType.SUCCESS;
+                }
+            }
+            player.swingArm(hand); //swings according to this method's return value
+        }
+        return ActionResultType.PASS;
+    }
+
+    protected boolean cycleGrowth(World world, BlockPos pos)
+    {
+        //try a thousand times to not have an infinite loop with a growable that can grow infinite times
+        for (int i = 0; i < tries; i++)
+        {
+            if (isValidToApply(world, pos))
+            {
+                doGrow(world, pos);
+                if (!isValidToApply(world, pos))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    protected void doGrow(World world, BlockPos pos)
+    {
+        BlockState blockState = world.getBlockState(pos);
+        if (blockState.getBlock() instanceof IGrowable)
+        {
+            ((IGrowable) blockState.getBlock()).grow(world, world.rand, pos, blockState);
+        }
+    }
+
+    protected boolean isValidToApply(World world, BlockPos pos)
+    {
+        BlockState blockState = world.getBlockState(pos);
+        if (blockState.getBlock() instanceof IGrowable)
+        {
+            return ((IGrowable) blockState.getBlock()).canGrow(world, pos, blockState, world.isRemote);
+        }
+        return false;
+    }
+}
