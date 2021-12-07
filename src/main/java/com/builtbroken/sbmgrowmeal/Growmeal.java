fd63f3675d194224c528a988a0520934f5dc355e
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-12-07 15:19:44 +0100

    code improvements

--- src/main/java/com/builtbroken/sbmgrowmeal/Growmeal.java
+++ src/main/java/com/builtbroken/sbmgrowmeal/Growmeal.java
@@ -1,8 +1,8 @@
 package com.builtbroken.sbmgrowmeal;
 
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -14,20 +14,18 @@ import net.minecraftforge.registries.ObjectHolder;
 @EventBusSubscriber(bus = Bus.MOD)
 public class Growmeal
 {
-    public static final String MODID = "sbmgrowmeal";
-    public static final String NAME = "[SBM] Growmeal";
-    public static final String PREFIX = MODID + ":";
+	public static final String MODID = "sbmgrowmeal";
+	public static final String PREFIX = MODID + ":";
 
-    public static final String ITEM_NAME = "growmeal";
+	public static final String ITEM_NAME = "growmeal";
 
-    @ObjectHolder(Growmeal.PREFIX + ITEM_NAME)
-    public static final Item GROWMEAL = null;
+	@ObjectHolder(Growmeal.PREFIX + ITEM_NAME)
+	public static final Item GROWMEAL = null;
 
-    @SubscribeEvent
-    public static void registerItems(RegistryEvent.Register<Item> event)
-    {
-        event.getRegistry().register(new ItemGrowmeal(new Item.Properties().tab(CreativeModeTab.TAB_MISC))
-                .setRegistryName(new ResourceLocation(Growmeal.MODID, ITEM_NAME)));
-    }
-
+	@SubscribeEvent
+	public static void registerItems(RegistryEvent.Register<Item> event)
+	{
+		event.getRegistry().register(new ItemGrowmeal(new Item.Properties().tab(CreativeModeTab.TAB_MISC))
+				.setRegistryName(new ResourceLocation(Growmeal.MODID, ITEM_NAME)));
+	}
 }
--- src/main/java/com/builtbroken/sbmgrowmeal/ItemGrowmeal.java
+++ src/main/java/com/builtbroken/sbmgrowmeal/ItemGrowmeal.java
@@ -1,95 +1,93 @@
 package com.builtbroken.sbmgrowmeal;
 
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.BonemealableBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
-import net.minecraft.server.level.ServerLevel;
-
-import net.minecraft.world.item.Item.Properties;
+import net.minecraft.world.level.block.BonemealableBlock;
+import net.minecraft.world.level.block.state.BlockState;
 
 /**
  * Created by Dark(DarkGuardsman, Robert) on 6/15/2019.
  */
 public class ItemGrowmeal extends Item
 {
-    public static int tries = 1000;
+	public static int tries = 1000;
 
-    public ItemGrowmeal(Properties properties)
-    {
-        super(properties);
-    }
+	public ItemGrowmeal(Properties properties)
+	{
+		super(properties);
+	}
 
-    @Override
-    public InteractionResult useOn(UseOnContext ctx)
-    {
-        final Level world = ctx.getLevel();
-        final BlockPos pos = ctx.getClickedPos();
-        final Player player = ctx.getPlayer();
-        final InteractionHand hand = ctx.getHand();
-        final ItemStack stack = ctx.getItemInHand();
+	@Override
+	public InteractionResult useOn(UseOnContext ctx)
+	{
+		final Level level = ctx.getLevel();
+		final BlockPos pos = ctx.getClickedPos();
+		final Player player = ctx.getPlayer();
+		final InteractionHand hand = ctx.getHand();
+		final ItemStack stack = ctx.getItemInHand();
 
-        //world.getBlockState(pos) is never saved as the state will not update otherwhise after applying the meal
-        if (isValidToApply(world, pos))
-        {
-            if (!world.isClientSide)
-            {
-                if (cycleGrowth(world, pos)) //make sure that the growable has grown before removing an item from the player
-                {
-                    world.levelEvent(2005, pos, 0); //particles
+		//world.getBlockState(pos) is never saved as the state will not update otherwhise after applying the meal
+		if (isValidToApply(level, pos))
+		{
+			if (!level.isClientSide)
+			{
+				if (cycleGrowth(level, pos)) //make sure that the growable has grown before removing an item from the player
+				{
+					level.levelEvent(2005, pos, 0); //particles
 
-                    if (!player.isCreative())
-                    {
-                        stack.shrink(1);
-                    }
+					if (!player.isCreative())
+					{
+						stack.shrink(1);
+					}
 
-                    return InteractionResult.SUCCESS;
-                }
-            }
-            player.swing(hand); //swings according to this method's return value
-        }
-        return InteractionResult.PASS;
-    }
+					return InteractionResult.SUCCESS;
+				}
+			}
+			player.swing(hand); //swings according to this method's return value
+		}
+		return InteractionResult.PASS;
+	}
 
-    protected boolean cycleGrowth(Level world, BlockPos pos)
-    {
-        //try a thousand times to not have an infinite loop with a growable that can grow infinite times
-        for (int i = 0; i < tries; i++)
-        {
-            if (isValidToApply(world, pos))
-            {
-                doGrow(world, pos);
-                if (!isValidToApply(world, pos))
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+	protected boolean cycleGrowth(Level level, BlockPos pos)
+	{
+		//try a thousand times to not have an infinite loop with a growable that can grow infinite times
+		for (int i = 0; i < tries; i++)
+		{
+			if (isValidToApply(level, pos))
+			{
+				doGrow(level, pos);
+				if (!isValidToApply(level, pos))
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 
-    protected void doGrow(Level world, BlockPos pos)
-    {
-        BlockState blockState = world.getBlockState(pos);
-        if (!world.isClientSide && blockState.getBlock() instanceof BonemealableBlock)
-        {
-            ((BonemealableBlock) blockState.getBlock()).performBonemeal((ServerLevel)world, world.random, pos, blockState);
-        }
-    }
+	protected void doGrow(Level level, BlockPos pos)
+	{
+		BlockState blockState = level.getBlockState(pos);
+		if (!level.isClientSide && blockState.getBlock() instanceof BonemealableBlock bonemealableBlock)
+		{
+			bonemealableBlock.performBonemeal((ServerLevel)level, level.random, pos, blockState);
+		}
+	}
 
-    protected boolean isValidToApply(Level world, BlockPos pos)
-    {
-        BlockState blockState = world.getBlockState(pos);
-        if (blockState.getBlock() instanceof BonemealableBlock)
-        {
-            return ((BonemealableBlock) blockState.getBlock()).isValidBonemealTarget(world, pos, blockState, world.isClientSide);
-        }
-        return false;
-    }
+	protected boolean isValidToApply(Level level, BlockPos pos)
+	{
+		BlockState blockState = level.getBlockState(pos);
+		if (blockState.getBlock() instanceof BonemealableBlock bonemealableBlock)
+		{
+			return bonemealableBlock.isValidBonemealTarget(level, pos, blockState, level.isClientSide);
+		}
+		return false;
+	}
 }
